0aa5cba7cdc19d20dfc6110ed27b602e69838ace
Chris Chou <chhschou@gmail.com>
2017-08-16 12:31:41 +1200

    add client test app initialstate

---
+++ tests/client/app.test.js
@@ -0,0 +1,17 @@
+import test from 'ava'
+import {shallow, mount} from 'enzyme'
+import 'jsdom-global/register'
+
+import React from 'react'
+import App from '../../client/components/App'
+
+
+test('app rendered', t => {
+  const wrapper = shallow(<App />)
+  t.is(wrapper.find('.app-container').length, 1)
+})
+
+test('app initial state', t => {
+  const wrapper = shallow(<App />)
+  t.is(wrapper.state(), {currentWeather: {}})
+})
