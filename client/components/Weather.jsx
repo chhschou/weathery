66762ac78ba46e75272a10222ae721fde35478a5
Chris Chou <chhschou@gmail.com>
2017-08-18 15:19:10 +1200

    make Weather component render conditional upon having a weather result to render

--- client/components/Weather.jsx
+++ client/components/Weather.jsx
@@ -2,19 +2,28 @@ import React from 'react'
 
 import { connect } from 'react-redux'
 
+function getImgSrc({coord}) {
+  const imgSrc = 'https://maps.googleapis.com/maps/api/staticmap?center=' + coord.lat + ',' + coord.lon + '&zoom=13&size=300x300&sensor=false'
+
+  return imgSrc
+}
+
 export const Weather = ({ weather }) => {
-  const imgSrc = 'https://maps.googleapis.com/maps/api/staticmap?center=' + weather.coord.lat + ',' + weather.coord.lon + '&zoom=13&size=300x300&sensor=false'
 
   return (
-    <div className='weather'>
-      <h2>Weather at {weather.name}</h2>
-      <img src={imgSrc} alt='location' />
-      {Object.keys(weather.main).map((k, i) => {
-        return (
-          <p key={i}>{k}: {weather.main[k]} </p>
-        )
-      })}
-    </div>
+    <div className='weather' >
+      {weather.name &&
+        <div className='weather-details'>
+          <h2>Weather at {weather.name}</h2>
+          <img src={getImgSrc(weather)} alt='location' />
+          {Object.keys(weather.main).map((k, i) => {
+            return (
+              <p key={i}>{k}: {weather.main[k]} </p>
+            )
+          })}
+        </div >
+      }
+    </div >
   )
 }
 
--- client/reducers/weather.js
+++ client/reducers/weather.js
@@ -1,7 +1,7 @@
 import * as types from '../actions'
 
 const initialState = {
-  name: '',
+  name: null,
   coord: {
     lat: null,
     lon: null
