dd1b983f647f08e55121c0d76ada060c295479b4
Chris Chou <chhschou@gmail.com>
2017-09-17 08:58:11 +1200

    update package json with json-server and postcss

---
+++ client/postcss.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    plugins: [
+        require('postcss-nesting'),
+        require('postcss-partial-import'),
+        require('postcss-custom-media'),
+        require('autoprefixer')
+    ]
+}
