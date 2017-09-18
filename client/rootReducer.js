e1ebb21cad93568a18c8bdcae72f4a367ef6f1e5
Chris Chou <chhschou@gmail.com>
2017-09-18 18:19:45 +1200

    fix issue with error reducer

--- client/rootReducer.js
+++ client/rootReducer.js
@@ -2,11 +2,11 @@ import { combineReducers } from 'redux'
 import weathers from './modules/weathers'
 import locations from './modules/locations'
 import settings from './modules/settings'
-import { error } from './reducers'
+import reducers from './reducers'
 
 export default combineReducers({
   weathers: weathers.reducer,
   settings: settings.reducer,
   locations: locations.reducer,
-  error
+  error: reducers.error
 })
