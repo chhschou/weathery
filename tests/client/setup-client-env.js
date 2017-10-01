4b0d49bd0554645729efaca61beae9b1e33a900d
Chris Chou <chhschou@gmail.com>
2017-10-01 16:24:00 +1300

    test: #7 - fix weathers action test

--- tests/client/setup-client-env.js
+++
@@ -1,5 +0,0 @@
-import nock from 'nock'
-
-afterEach('remove all nock bindings', () => {
-  nock.cleanAll()
-})
--- tests/client/weathers.actions.test.js
+++ tests/client/weathers.actions.test.js
@@ -3,37 +3,58 @@ import configureMockStore from 'redux-mock-store'
 import thunk from 'redux-thunk'
 import 'dotenv/config'
 
-// import './setup-client-env'
-import { getConditions } from '../data/wu'
+import { getConditions, getForecastDays, getForecastHours } from '../data/wu'
 import weathers from 'modules/weathers'
-import { getBase, getApiFragment } from '../../common/url_builders/wu'
+import { getBase, getApiFragment } from 'url_builders/wu'
+import rawResponse from '../data/wu.json'
+import adapter from 'adapters/wu'
 
 const middleware = [thunk]
 const mockStore = configureMockStore(middleware)
-
-test('getWeatherViaLocationId action creator works', () => {
-  nock(getBase())
-    .get(getApiFragment('conditions', process.env.WUNDERGROUND_APIKEY, -41.29, 174.78))
-    .reply(200, getConditions())
-
-  const expectedActionCreators = [
-    { type: weathers.actionTypes.REQUEST, locationId: 1 },
-    { type: weathers.actionTypes.RECEIVE, locationId: 1, weather: getConditions() }]
-
-  const store = mockStore({
-    locations: {
-      items: [undefined, { id: 1, coords: { lat: -41.29, lng: 174.78 } }]
-    },
-    weathers: {
-      items: {}
-    },
-    settings: {
-      weatherApiKey: process.env.WUNDERGROUND_APIKEY,
-    }
+describe('weathers actions', () => {
+  afterEach(() => {
+    nock.cleanAll()
   })
 
-  return store.dispatch(weathers.actions.getWeatherViaLocationId(1))
-    .then(() => {
-      expect(store.getActions()).toEqual(expectedActionCreators)
-    });
+  test('getWeatherViaLocationId action creator works', () => {
+    nock(getBase())
+      .get(getApiFragment('conditions', process.env.WUNDERGROUND_APIKEY, -41.29, 174.78))
+      .reply(200, rawResponse.conditions)
+    nock(getBase())
+      .get(getApiFragment('hourly10day', process.env.WUNDERGROUND_APIKEY, -41.29, 174.78))
+      .reply(200, rawResponse.h10)
+    nock(getBase())
+      .get(getApiFragment('forecast10day', process.env.WUNDERGROUND_APIKEY, -41.29, 174.78))
+      .reply(200, rawResponse.f10)
+
+    const expectedActionCreators = [
+      { type: weathers.actionTypes.REQUEST, locationId: 1 },
+      {
+        type: weathers.actionTypes.RECEIVE,
+        locationId: 1,
+        weather: {
+          currentConditions: getConditions(),
+          f10: getForecastDays(),
+          h10: getForecastHours()
+        }
+      }
+    ]
+
+    const store = mockStore({
+      locations: {
+        items: [undefined, { id: 1, coords: { lat: -41.29, lng: 174.78 } }]
+      },
+      weathers: {
+        items: {}
+      },
+      settings: {
+        weatherApiKey: process.env.WUNDERGROUND_APIKEY,
+      }
+    })
+
+    return store.dispatch(weathers.actions.getWeatherViaLocationId(1))
+      .then(() => {
+        expect(store.getActions()).toEqual(expectedActionCreators)
+      })
+  })
 })
