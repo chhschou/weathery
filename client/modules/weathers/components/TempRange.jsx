2925db1fbcfd2d69570eedf7ca47b11d13e58d44
Chris Chou <chhschou@gmail.com>
2017-10-07 14:10:38 +1300

    feat: #7 - add style for TempRange

--- client/modules/weathers/components/TempRange.jsx
+++ client/modules/weathers/components/TempRange.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 
 import Temp from './objects/Temp'
+import styles from './TempRange.css'
 
-export default function ({ isCelsius, range }) {
+export default function ({ isRow, isCelsius, range }) {
   const { highC, lowC, highF, lowF } = range
+  const classes = isRow ? 'c-temprange is-row' : 'c-temprange'
   return (
-    <div className='c-temprange'>
+    <div className={classes}>
       <Temp isCelsius={isCelsius} c={highC} f={highF} />
-      <span> | </span>
       <Temp isCelsius={isCelsius} c={lowC} f={lowF} />
     </div>
   )
