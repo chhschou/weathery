1bf82f15c731bbbdce96288dbb75b4fd819b55eb
Chris Chou <chhschou@gmail.com>
2017-10-12 22:30:17 +1300

    test($client): #22 - Create a test to ensure autocomplete works
    #22

--- tests/client/locations.actions.test.js
+++ tests/client/locations.actions.test.js
@@ -1,1 +1,50 @@
-test('placeholder', () => {})
+import request from 'superagent'
+import nock from 'nock'
+import Rx from 'rxjs/Rx'
+
+afterEach(() => {
+  nock.cleanAll()
+})
+
+test('autocomplete works', (done) => {
+  const base = 'https://autocomplete.wunderground.com'
+  const interrupt = Rx.Observable.create((observer) => {
+    let token = 0
+    setInterval(() => {
+      token++
+      observer.next(token)
+    }, 0)
+  }).take(3)
+
+
+  const doApi = (token) => {
+    const op = Rx.Observable.create((observer) => {
+      const frag = `/aq?query=${token}`
+      nock(base)
+        .get(frag)
+        .delay(120)
+        .reply(200, token)
+
+      const req = request.get(base + frag)
+        .end((err, res) => {
+          if (err) observer.error(err)
+          observer.next(res)
+          observer.complete()
+        })
+
+      return () => {
+        req.abort()
+      }
+    })
+
+    return op
+  }
+
+
+  const searcher = interrupt.switchMap(doApi)
+  searcher.subscribe(
+    (res) => { expect(res.body).toBe(3) },
+    (err) => console.log(err),
+    () => done()
+  )
+})
