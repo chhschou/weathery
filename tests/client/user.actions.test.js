53bbe305b15e442f729ce51b93a15206ae9883c8
Chris Chou <chhschou@gmail.com>
2017-10-12 22:28:15 +1300

    test($client): #22 - cancellable request
    call back only

--- tests/client/user.actions.test.js
+++ tests/client/user.actions.test.js
@@ -2,12 +2,14 @@ import nock from 'nock'
 import configureMockStore from 'redux-mock-store'
 import thunk from 'redux-thunk'
 import 'dotenv/config'
+import Rx from 'rxjs/Rx'
+import request from 'superagent'
 
 import { getLocation, getConditions } from '../data/wu'
 import weathers from 'modules/weathers'
 import locations from 'modules/locations'
 import user from 'modules/user'
-import { getBase, getApiFragment } from 'url_builders/wu'
+import { getConditionsUrl, getBase, getApiFragment } from 'url_builders/wu'
 import rawResponse from '../data/wu.json'
 import adapter from 'adapters/wu'
 
@@ -53,8 +55,34 @@ describe('user actions', () => {
     })
 
     return store.dispatch(user.actions.getUserLocationAndWeather(lat, lng, locationId))
-      .then(() => {
-        expect(store.getActions()).toEqual(expectedActionCreators)
-      })
+      .then(() => expect(store.getActions()).toEqual(expectedActionCreators))
   })
+})
+
+test('abort getCondition', (done) => {
+  const lat = -41.29, lng = 174.78, locationId = 0
+  nock(getBase())
+    .get(getApiFragment('conditions', process.env.WUNDERGROUND_APIKEY, lat, lng))
+    .delay(1000)
+    .reply(200, 56)
+
+  const res$ = Rx.Observable.create((observer) => {
+    const url = getConditionsUrl(lat, lng, process.env.WUNDERGROUND_APIKEY)
+    const req = request.get(url)
+      .on('abort', () => {
+        done()
+      })
+      .end((err, result) => {
+      })
+
+    return () => {
+      req.abort()
+    }
+  })
+
+  const sub = res$.subscribe()
+
+  setTimeout(() => {
+    sub.unsubscribe()
+  }, 200)
 })
